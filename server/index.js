eaeb98d860c382642e8368f2c144c0be26893a2d
Cassels-null <42cassels@gmail.com>
2019-09-26 14:29:46 -0400

    Allow API to search youtube vids

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const parser = require('body-parser');
 const express = require('express');
-
-const scribe = require('./scribe.js');
+const axios = require('axios');
+const YOUTUBE_API_KEY = require('./youtube.js');
 
 const app = express();
 const port = 1337;
@@ -17,8 +17,21 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.post('/record', scribe.record);
-app.get('/read', scribe.read);
+app.get('/search', searchYouTube);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+
+function searchYouTube(req,res){
+
+  let searchString = 'https://www.googleapis.com/youtube/v3/search?q=XCOMshiv&part=snippet&key='+YOUTUBE_API_KEY+'&maxResults=10&type=video&videoEmbeddable=true'
+  axios.get(searchString)
+  .then((result)=>{
+    // console.log(result.data.items);
+    res.status(200).send(result.data.items);
+  })
+  .catch((err)=>{
+    console.log(err);
+    res.send(200);
+  });
+}
